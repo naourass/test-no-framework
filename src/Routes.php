23683aa04ab6a1d846f5b88d7f4970a701433b29
naourass derouichi <naourass derouichi>
2016-04-26 11:19:29 +0100

    fifth commmit

--- src/Routes.php
+++ src/Routes.php
@@ -1,10 +1,5 @@
 <?php
 
 return [
-    ['GET', '/hello-world', function () {
-        echo 'Hello World';
-    }],
-    ['GET', '/another-route', function () {
-        echo 'This works too';
-    }],
+    ['GET', '/', ['Example\Controllers\Homepage', 'show']],
 ];
